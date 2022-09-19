7808e9acaed8803012c7ee9cd5cb66af7380a953
TMC\mehrnoosh.rigi <mehrnoosh.rigi@tmc.nl>
2022-09-19 10:16:40 +0200

    java annotations: setter injection

--- src/spring/demo/annotations/TennisCoach.java
+++ src/spring/demo/annotations/TennisCoach.java
@@ -7,10 +7,23 @@ import org.springframework.stereotype.Component;
 public class TennisCoach implements Coach{
     private FortuneService fortuneService;
 
+    //define default constructor
+    public TennisCoach(){
+        System.out.println("TennisCoach: inside default constructor");
+    }
+
+    // define setter method
     @Autowired
-    public TennisCoach(FortuneService theFortuneService){
+    public void setFortuneService(FortuneService theFortuneService){
+        System.out.println("TennisCoach: inside setFortuneService");
         this.fortuneService = theFortuneService;
     }
+
+//    @Autowired
+//    public TennisCoach(FortuneService theFortuneService){
+//
+//        this.fortuneService = theFortuneService;
+//    }
     @Override
     public String getDailyMethod() {
         return "Get daily method from class TennisCoach which implements Coach interface";
